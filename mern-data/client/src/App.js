59e0f40961e0796fd3159e577079d961cca88c7f
greglamm <glamm@csi.edu>
2022-04-17 19:44:17 -0600

    Milestone_Success
    Only half-working. Need to get data to display on second page.

--- mern-data/client/src/App.js
+++ mern-data/client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./App.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -11,17 +10,15 @@ import {
 } from "./components";
 
 const App = () => {
-ReactDOM.render(
-  <Router>
-    <Navigation />
-    <Routes>
-      <Route path="/" component={<Home />} />
-      <Route path="/data" component={<Data />} />
-    </Routes>
-    <Footer />
-  </Router>,
-
-  document.getElementById("root")
+  return (
+    <Router>
+      <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/data" element={<Data />} />
+        </Routes>
+      {/* <Footer /> */}
+    </Router>
 );
 };
 
--- mern-data/client/src/components/Home.jsx
+++ mern-data/client/src/components/Home.jsx
@@ -4,17 +4,17 @@ import Info from './Info';
 function Home() {
     return (
       <div className="home">
-        <div class="container">
-          <div class="row align-items-center my-5">
-            <div class="col-lg-7">
+        <div className="container">
+          <div className="row align-items-center my-5">
+            <div className="col-lg-7">
               <img
-                class="img-fluid rounded mb-4 mb-lg-0"
+                className="img-fluid rounded mb-4 mb-lg-0"
                 src="http://placehold.it/900x400"
                 alt=""
               />
             </div>
-            <div class="col-lg-5">
-              <h1 class="font-weight-light">Home</h1>
+            <div className="col-lg-5">
+              <h1 className="font-weight-light">Home</h1>
               <h2>Bringing your everyday lawncare needs to you!</h2>
           <h3>About</h3>
             <p>
